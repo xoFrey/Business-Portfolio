7e77f4d908d135a86656b1e251c578ef448fa28d
xofrey <izel.acxr@gmail.com>
2026-05-12 01:41:18 +0200

    projects done

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,28 +1,52 @@
 import "./CSS/ProjectItem.css";
+import { LiaLongArrowAltRightSolid } from "react-icons/lia";
 
-const ProjectItem = () => {
+const ProjectItem = ({
+  date,
+  typeOfProject,
+  title,
+  description,
+  techstack,
+  imgUrl,
+  link,
+}) => {
   return (
-    <section className='project-item'>
-      <p className='paragraph'>About</p>
-      <div>
-        <p>2024 - Solo Project</p>
-        <div className='image-placeholder'>
-          <p>[PokeDex App]</p>
+    <>
+      <section className='project-item'>
+        {/* <p className='paragraph'>Projects</p> */}
+        <div className='project-hero'>
+          <div className='project-img'>
+            <p>
+              {date} - {typeOfProject}
+            </p>
+
+            <div className='project-img-placeholder '>
+              <p>[{title}]</p>
+            </div>
+          </div>
+          <div className='project-info'>
+            <div className='project-headline'>
+              <h3>{title}</h3>
+              <LiaLongArrowAltRightSolid
+                className='icon'
+                fill='#c97c20'
+                size={"30px"}
+              />
+            </div>
+
+            <p className='body-text'>
+              Search and explore Pokémon with full stats, evolutions, and type
+              filtering. Live PokéAPI integration.
+            </p>
+            <div className='techstack'>
+              {techstack.map((item, index) => (
+                <p key={index}>{item}</p>
+              ))}
+            </div>
+          </div>
         </div>
-      </div>
-      <div>
-        <h3>Pokedex App</h3>
-        <p>
-          Search and explore Pokémon with full stats, evolutions, and type
-          filtering. Live PokéAPI integration.
-        </p>
-        <div>
-          <p>React</p>
-          <p>API</p>
-          <p>CSS</p>
-        </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,32 @@
 import ProjectItem from "./ProjectItem";
+import "./CSS/ProjectItem.css";
 
 const Projects = () => {
   return (
-    <section>
-      <ProjectItem />
+    <section className='projects'>
+      <p className='paragraph'>Projects</p>
+      <ProjectItem
+        date={"2024"}
+        typeOfProject={"Solo Project"}
+        title={"Pokedex App"}
+        description={
+          "Search and explore Pokémon with full stats, evolutions, and type filtering. Live PokéAPI integration."
+        }
+        techstack={["React", "API", "CSS"]}
+        imgUrl={"Placeholder"}
+        link={"Github"}
+      />
+      <ProjectItem
+        date={"2025"}
+        typeOfProject={"Team Project"}
+        title={"Silent Moon App"}
+        description={
+          "Search and explore Pokémon with full stats, evolutions, and type filtering. Live PokéAPI integration."
+        }
+        techstack={["React", "API", "CSS", "Express", "Mongoose"]}
+        imgUrl={"Placeholder"}
+        link={"Github"}
+      />
     </section>
   );
 };
