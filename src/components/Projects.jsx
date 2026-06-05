00bf4e49a34a41e2a30b7b84db9a99def568433a
xofrey <izel.acxr@gmail.com>
2026-06-05 15:55:07 +0200

    design improved

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,28 +5,30 @@ const Projects = () => {
   return (
     <section className='projects'>
       <p className='paragraph'>Projects</p>
-      <ProjectItem
-        date={"2024"}
-        typeOfProject={"Solo Project"}
-        title={"Pokedex App"}
-        description={
-          "Search and explore Pokémon with full stats, evolutions, and type filtering. Live PokéAPI integration."
-        }
-        techstack={["React", "API", "CSS"]}
-        imgUrl={"Placeholder"}
-        link={"Github"}
-      />
-      <ProjectItem
-        date={"2025"}
-        typeOfProject={"Team Project"}
-        title={"Silent Moon App"}
-        description={
-          "Search and explore Pokémon with full stats, evolutions, and type filtering. Live PokéAPI integration."
-        }
-        techstack={["React", "API", "CSS", "Express", "Mongoose"]}
-        imgUrl={"Placeholder"}
-        link={"Github"}
-      />
+      <div>
+        <ProjectItem
+          date={"2024"}
+          typeOfProject={"Solo Project"}
+          title={"Pokedex App"}
+          description={
+            "Search and explore Pokémon with full stats, evolutions, and type filtering. Live PokéAPI integration."
+          }
+          techstack={["React", "API", "CSS"]}
+          imgUrl={"Placeholder"}
+          link={"Github"}
+        />
+        <ProjectItem
+          date={"2025"}
+          typeOfProject={"Team Project"}
+          title={"Silent Moon App"}
+          description={
+            "Search and explore Pokémon with full stats, evolutions, and type filtering. Live PokéAPI integration."
+          }
+          techstack={["React", "API", "CSS", "Express", "Mongoose"]}
+          imgUrl={"Placeholder"}
+          link={"Github"}
+        />
+      </div>
     </section>
   );
 };
